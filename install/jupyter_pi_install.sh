d442bd37791ddad9ffb38a1fb7775011b226a82d
esumitra <edward.sumitra@gmail.com>
2016-06-22 22:49:06 -0400

    added autoinstall for python-pil

--- install/jupyter_pi_install.sh
+++ install/jupyter_pi_install.sh
@@ -4,7 +4,7 @@
 sudo apt-get -y update
 sudo apt-get -y install build-essential python-dev python-distlib python-setuptools python-pip python-wheel libzmq-dev libgdal-dev
 sudo apt-get -y install xsel xclip libxml2-dev libxslt-dev python-lxml python-h5py python-numexpr python-dateutil python-six python-tz python-bs4 python-html5lib python-openpyxl python-tables python-xlrd python-xlwt cython python-sqlalchemy python-xlsxwriter python-jinja2 python-boto python-gflags python-googleapi python-httplib2 python-zmq libspatialindex-dev
-sudo apt-get install python-requests python-pil python-scrapy python-geopy python-shapely python-pyproj
+sudo apt-get -y install python-requests python-pil python-scrapy python-geopy python-shapely python-pyproj
 
 # pip installs
 yes | sudo pip install -U pip
